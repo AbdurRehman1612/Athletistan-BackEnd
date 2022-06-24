f1e997e72d27fc02989fb720ec782937cc8987fc
Abdur Rehman <abdurrehmanzab@gmail.com>
2022-06-25 00:51:27 +0500

    abc

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const originURL = ["https://athletistan.herokuapp.com"];
+// const originURL = ["https://athletistan.herokuapp.com"];
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
 
 // app.use(express.urlencoded({ extended: false }));
 // app.use(express.json());
 
-app.use(
-  cors({
-    origin: originURL,
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: originURL,
+//     credentials: true,
+//   })
+// );
+app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
